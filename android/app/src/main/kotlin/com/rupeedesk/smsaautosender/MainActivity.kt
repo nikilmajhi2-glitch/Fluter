b685b7ad93989eebed6b84436e60148b05d523e4
nikilmajhi2-glitch <nikhilmajhi2@gmail.com>
2025-11-06 19:33:11 +0530

    update

--- android/app/src/main/kotlin/com/rupeedesk/smsaautosender/MainActivity.kt
+++ android/app/src/main/kotlin/com/rupeedesk/smsaautosender/MainActivity.kt
@@ -2,5 +2,4 @@ package com.rupeedesk.smsaautosender
 
 import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterFragmentActivity() {
-}
+class MainActivity : FlutterFragmentActivity()
